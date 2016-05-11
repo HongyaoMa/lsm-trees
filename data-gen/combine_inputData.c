f22c297a2a20f6bf11784d29893449151795a62e
HongyaoMa <mahongyao@gmail.com>
2016-05-11 00:23:53 -0400

    generating testing data!

--- data-gen/combine_inputData.c
+++ data-gen/combine_inputData.c
@@ -7,7 +7,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define NUM_FILES 1
+#include <string.h>
+
+#define NUM_FILES 10
 
 int main() {
 
@@ -18,37 +20,104 @@ int main() {
 	// Read the data
 	int inputKey;
 	long inputValue;
-	FILE *fp;
+	FILE *fp_out;
+	FILE ** inputFiles = malloc(sizeof(FILE*) * NUM_FILES);
+
+	FILE *fp_in;
+
+	// Parts of the filename
+	char file_name_part_1[] = "output";
+	char file_name_part_2[] = "0";
+	char file_name_part_3[] = ".csv";
+
+	printf("%s\n", file_name_part_1);
+	printf("%s\n", file_name_part_2);
+	printf("%s\n", file_name_part_3);		
+
+	char *fileName_combined = malloc(strlen(file_name_part_1) + strlen(file_name_part_2) + strlen(file_name_part_3) +1);
+
 
 	for (i_file = 0; i_file < NUM_FILES; i_file++){
-		fp = fopen("data_100Mpairs.csv","r");
-		if (fp == NULL){
+		snprintf(file_name_part_2, 2, "%d", i_file);
+
+		strcpy(fileName_combined, file_name_part_1);
+		strcat(fileName_combined, file_name_part_2);
+		strcat(fileName_combined, file_name_part_3);
+		
+		// *(inputFiles + i_file) = malloc(sizeof(FILE *));	
+		// *(inputFiles + i_file) = fopen(fileName_combined,"r");
+		inputFiles[i_file] = fopen(fileName_combined,"r");
+		// fp_in = fopen(fileName_combined,"r");
+
+		if (inputFiles[i_file] == NULL){
 			fprintf(stderr, "Failed to open input data file %d!\n", i_file);
 		}
+		else{
+			printf("Successfully opened file %s\n", fileName_combined);
+		}
+
+		printf("%p\n",inputFiles[i_file]);
+		
 	}
 
-	/* 
+	fp_out = fopen("output_combined.csv","w");
+	if (fp_out == NULL){
+		printf("Failed to create the output file!\n");
+	}
+
+	int inds[NUM_FILES];
+
+	for (i_file = 0; i_file < NUM_FILES; i_file++){
+		inds[i_file] = 0;
+	}
+
+
+	int total_num = 0;
+	long iter_count = 0;
+	int rand_ind;
+
+
 	// The timer:
 	clock_t begin, end;
 	double time_spent;
 
 	// Filling up the tree
-	begin = clock();	
-	for (i=0; i < totalSize; i++){
-		fscanf(fp, "%d, %ld", &inputKey, &inputValue);		
-		put_with_key(testTree, inputKey, inputValue);	
-	}
-	fclose(fp);
+	begin = clock();
 
-	// Recored the time
+	while (total_num <  individual_size * NUM_FILES){
+		
+		rand_ind = rand()%NUM_FILES;
+		
+		if (inds[rand_ind] < individual_size){
+
+			fscanf(inputFiles[rand_ind], "%d, %ld", &inputKey, &inputValue);
+			fprintf(fp_out, "%d, %ld\n", inputKey, inputValue);
+
+			inds[rand_ind]++;
+			total_num ++;
+			iter_count ++;
+		}
+		else{
+			iter_count ++;
+			continue;
+		}
+	}
+	printf("Total number of iterations is %ld\n", iter_count);
 	end = clock();
 	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("Construction of the tree with %d elements took %f seconds!\n", totalSize, time_spent);
+	printf("Combining the files took %f seconds!\n", time_spent);
 
-	// Print the meta data
-	print_meta_data(testTree);
+	free(fileName_combined);
 
+	/* TODO: Valgrind's error for closing
+	for (i_file = 0; i_file< NUM_FILES; i_file++){
+		close(*(inputFiles + i_file));
+	}
+	free(inputFiles);
 	*/
+	fclose(fp_out);
+
+
 
 	return 0;
 }
--- data-gen/make_file.py
+++ data-gen/make_file.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-n_lines = 1*10**8
-max_value = 10**10
+n_lines = 1*10**1
+max_value = 10**2
 
 A = range(n_lines)
 A = np.random.permutation(A) + 9*n_lines
@@ -10,7 +10,7 @@ B = np.random.randint(max_value, size=len(A))
 import time
 t0 = time.time()
 
-with open("output10.csv", 'w') as f:
+with open("output_small_9.csv", 'w') as f:
     for i, b in zip(A, B):
         f.write('{}, {}\n'.format(i, b))
 
